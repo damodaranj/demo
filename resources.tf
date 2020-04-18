711ba434698cd539c7fea876d9d9ccdba862f4ed
damodaran <damokrishan@gmail.com>
2020-04-18 16:13:46 +0000

    this is test

--- resources.tf
+++ resources.tf
@@ -9,19 +9,19 @@ resource "aws_s3_bucket_object" "default" {
 }
 
 resource "aws_elastic_beanstalk_application" "default" {
-  name        = "devops-node-materials1"
-  description = "devops-node description"
+  name        = "damo-node-materials"
+  description = "damo-node description"
 }
 
 resource "aws_elastic_beanstalk_application_version" "default" {
-  name        = "devops-node-materials1-label"
-  application = "devops-node-materials1"
+  name        = "damo-node-materials-label"
+  application = "damo-node-materials"
   description = "application version created by terraform"
   bucket      = "${aws_s3_bucket.default.id}"
   key         = "${aws_s3_bucket_object.default.id}"
 }
 resource "aws_elastic_beanstalk_environment" "tfenvtest" {
-  name                = "devops-node-materials1-env"
+  name                = "damo-node-materials-env"
   application         = "${aws_elastic_beanstalk_application.default.name}"
   solution_stack_name = "64bit Amazon Linux 2 v3.0.0 running Docker"
   #solution_stack_name = "64bit Amazon Linux 2018.03 v4.13.0 running Node.js"
